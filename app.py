cb2d03f246acc673d7c043fce5ac8f47204e4691
Koval Yaroslav <koval@q-writer.com>
2021-10-14 14:27:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import logging
+import logging, re
 
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
@@ -9,6 +9,47 @@ from time import time
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+
+
+def cleanhtml(raw_html) -> str:
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+def fix_string(string) -> str:
+    in_word = string
+    in_between_words = ['-', '–']
+    in_sentences = ['«', '(', '[', '{', '"', '„', '\'']
+    string = string.replace("\n\n", "\n")
+    for item in in_between_words:
+        regex = r'\w[%s]\s\w' % item
+        in_word = re.findall(regex, string)
+
+        for x in in_word:
+            a = x[:1]; b = x[3:4]
+            string = string.replace(x, a + '-' + b)
+
+    for item in in_sentences:
+        string = string.replace(f' {item} ', f' {item}')
+    return string
+
+
+def decode_story_string(array) -> str:
+    struct_array = []
+    array = json.loads(array)
+    for i in array:
+        text = cleanhtml(i[0])
+        text = fix_string(text)
+        if check_long_words_in_string(text):
+            text = text.replace('\n', '</br>')
+            if i[1]:
+                struct_array.append('<b>%s</b>' % text)
+            else: 
+                struct_array.append('<i>%s</i>' % text)
+        else:
+            struct_array.append('<b>Произошла ошибка!</b>')
+    return ''.join(struct_array)
 
 
 class status(Resource):
@@ -27,6 +68,9 @@ class Random(Resource):
           }
         ).json()
         json_data = response["data"]
+        
+        for el in json_data:
+            el["body"] = decode_story_string(el["body"])
         
         return jsonify(json_data)
 
