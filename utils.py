f54bb215d9a4719c1b02fdf3596caf8aeb14618d
Koval Yaroslav <koval@q-writer.com>
2021-11-10 18:53:15 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -51,6 +51,7 @@ def decode_story_string(array: list) -> str:
         ) for el in json.loads(array) if check_long_words_in_string(el[0])
     ]
     
+    print(array)
     result = remove_empty(list(map(
         lambda x: "<b>%s</b>" % x[0] if x[1] else "<i>%s</i>" % x[0], array
     )))
